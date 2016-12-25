5849cc88cbaa83a0731aa7a44548de43988afc4e
Steven Halase <stevenhalase@Stevens-MacBook-Pro.local>
2016-12-25 12:58:53 -0600

    Revert "Revert "Initial API""
    This reverts commit dac68a7f4e2570fe465678b7cbf89373a5c8f73e.

---
+++ User/UserRoutes.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+var UserController = require('./UserController.js');
+
+/*
+ * GET
+ */
+router.get('/api/users/', UserController.list);
+
+/*
+ * GET
+ */
+router.get('/api/users/:id', UserController.show);
+
+/*
+ * GET
+ */
+router.post('/api/users/login', UserController.login);
+
+/*
+ * POST
+ */
+router.post('/api/users/', UserController.create);
+
+/*
+ * PUT
+ */
+router.put('/api/users/:id', UserController.update);
+
+/*
+ * DELETE
+ */
+router.delete('/api/users/:id', UserController.remove);
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const request = require('request');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
+const port = process.env.PORT || 4000;
+
+const UserRoutes = require('./User/UserRoutes');
+
+const uristring =
+    process.env.MONGODB_URI ||
+    'mongodb://localhost/comeeda';
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, './www')))
+
+mongoose.connect(uristring, function(error) {
+  if (error) {
+      console.error(error);
+  } else {
+      console.log('Mongoose connected successfully')
+  }
+})
+
+app.use('/', UserRoutes);
+
+app.listen(port, function () {
+    console.log('Server started at localhost:' + port);
+})
