3602636a37b6446038af8e57caa473eea1b9f865
Steven Halase <stevenhalase@Stevens-MacBook-Pro.local>
2016-12-27 23:49:15 -0600

    Added res methods

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
 });
 app.use(express.static(path.join(__dirname, './www')))
