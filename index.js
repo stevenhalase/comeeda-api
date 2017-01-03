ac660631cb64125699ce9d28e288aec0621f4dea
Steven Halase <stevenhalase@Stevens-MacBook-Pro.local>
2017-01-02 21:15:00 -0600

    Added allow credentials header

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
